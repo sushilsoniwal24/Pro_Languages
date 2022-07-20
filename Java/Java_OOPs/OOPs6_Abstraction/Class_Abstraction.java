9b87f96f3e5027c106ca0656f21a5379f92311bf
Sushil Kumar Soniwal <soniwalsushil24@gmail.com>
2022-07-20 18:26:12 +0530

    Abstraction in java (Abstract Class)

---
+++ Java/Java_OOPs/OOPs6_Abstraction/Class_Abstraction.java
@@ -0,0 +1,27 @@
+package Java.Java_OOPs.OOPs6_Abstraction;
+
+abstract class Birds {
+    abstract void color();
+}
+
+class Parrot extends Birds {
+    void color() {
+        System.out.println("Parrot's color is green");
+    }
+}
+
+class Crow extends Birds {
+    void color() {
+        System.out.println("Crow's color is black");
+    }
+}
+
+public class Class_Abstraction {
+    public static void main(String[] args) {
+        Parrot pt = new Parrot();
+        pt.color();
+
+        Crow cr = new Crow();
+        cr.color();
+    }
+}
